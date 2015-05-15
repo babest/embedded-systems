d8dc399dfe6b8f43dfc3f0cf27c2cbb39ad5c470
likesmusik <3back@informatik.uni-hamburg.de>
2015-05-15 16:37:59 +0200

    Blatt03 - aufgabe 3.4 untestet

--- Blatt03/aufgabe3.4/arduino3.4.ino
+++ Blatt03/aufgabe3.4/arduino3.4.ino
@@ -1,12 +1,18 @@
 #include <Servo.h>
 
+#define STRINGIZE_NX(A) #A
+#define STRINGIZE(A) ((A),STRINGIZE_NX(A))
+
+
 int servoOutputPin = 3;
 Servo servo;
-int servoPos = 0;
+//int servoPos = 0;
 int servoMax = 180;
-int servoAdjust = 23; // 23 - 157°
+//int servoAdjust = 23; // 23 - 157°
 
+//Serial Input is saved here
 char serialData[100];
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,23 +26,58 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   
+  //Got characters?
   if(0 < Serial.available()) {
+	//Read them 
     Serial.readBytesUntil('\n', serialData, 100);
     
-    if (strstr(serialData, "LED_on") != null) {
-      LED_on();
+	//Parse for commands and run them
+    if (strstr(serialData, "LED_on") != NULL) {
+      //Run it
+	  LED_on();
     }
-    else if (strstr(serialData, "LED_off") != null) {
-      LED_off();
+    else if (strstr(serialData, "LED_off") != NULL) {
+      //Run it
+	  LED_off();
     }
-    else if (strstr(serialData, "moveTo(") != null) {
-      int value = atoi(serialData+strlen("moveTo("));
-      moveTo(value);
-      //TODO: weiterschreiben
+    else if (strstr(serialData, "moveTo") != NULL) {
+	  char* start, end;
+	  start = strchr(serialData, '(')+1;//TODO: recheck if +1
+	  end   = strchr(serialData, ')')-1;//TODO: recheck if -1
+	  
+	  if(start != NULL && end != NULL && start < end) {
+		//We have found a 'valid' beginning and end
+		
+		//Check if in between the brackets are only digits
+		while(start != end) {
+		  if( !isdigit(end[0])) {
+		    break;
+		  }
+		  end--;
+		}
+		
+		if(start == end) {
+		  //So, there are only digits
+		  
+		  //Convert the text to a number
+		  int value = atoi(start);
+		  //Run it
+		  moveTo(value);
+		}
+		else {
+		  Serial.println("moveTo-Kommando falsch. Richtig: moveTo(ZAHL), wobei ZAHL im Bereich von 0 bis " STRINGIZE(servoMax) " liegt (-5.1)");
+		}
+	  }
+	  else {
+	    Serial.println("moveTo-Kommando falsch. Richtig: moveTo(ZAHL), wobei ZAHL im Bereich von 0 bis " STRINGIZE(servoMax) " liegt (-5.0)");
+	  }
     }
+	else {
+	  //No command found...
+	  Serial.print("Kommando nicht verstanden (-1): ");
+	  Serial.println(serialData);
+	}
   }
-  
-  servo.write(servoPos);
 }
 
 
@@ -46,4 +87,13 @@ void LED_on() {
 
 void LED_off() {
   digitalWrite(LEDPin, LOW);
+}
+
+void moveTo(int angle) {
+  if(0 <= angle && angle <= servoMax) {
+    servo.write(angle);
+  }
+  else {
+	Serial.println("moveTo-Kommando falsch. angle is either to big or too small.");
+  }
 }
